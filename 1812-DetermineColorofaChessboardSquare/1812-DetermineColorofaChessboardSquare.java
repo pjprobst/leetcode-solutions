8c9f53658c079a339b171d16edc4befa255a1d63
pjprobst <111828284+pjprobst@users.noreply.github.com>
2025-09-16 19:04:10 -0400

    Create 1812-DetermineColorofaChessboardSquare

---
+++ 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.java
@@ -0,0 +1,14 @@
+// Last updated: 9/16/2025, 7:04:10 PM
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        char letter = coordinates.charAt(0);
+        int num = Integer.parseInt(coordinates.substring(1,2)); 
+        if((letter=='a'||letter=='c'||letter=='e'||letter=='g')&&num%2==1){
+            return false;
+        }
+        else if((letter=='b'||letter=='d'||letter=='f'||letter=='h')&&num%2==0){
+            return false;
+        }
+        return true;
+    }
+}
