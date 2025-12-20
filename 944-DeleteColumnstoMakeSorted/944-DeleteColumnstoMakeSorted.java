fe0d6f9170bfd6466f3efcaa4b90ded059593543
pjprobst <111828284+pjprobst@users.noreply.github.com>
2025-12-20 17:07:43 -0500

    File updated at 12/20/2025, 5:07:42 PM

--- 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.java
+++ 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.java
@@ -1,16 +1,15 @@
-// Last updated: 12/20/2025, 5:07:32 PM
+// Last updated: 12/20/2025, 5:07:42 PM
 1class Solution {
 2    public int minDeletionSize(String[] strs) {
-3        boolean sorted = true;
-4        int count = 0;
-5        for (int i = 0; i<strs[0].length(); i++){
-6            for (int j = 1; j<strs.length; j++){
-7                if (strs[j-1].charAt(i) > strs[j].charAt(i)){
-8                    count++;
-9                    break;
-10                }
-11            }
-12        }
-13        return count;
-14    }
-15}
+3        int count = 0;
+4        for (int i = 0; i<strs[0].length(); i++){
+5            for (int j = 1; j<strs.length; j++){
+6                if (strs[j-1].charAt(i) > strs[j].charAt(i)){
+7                    count++;
+8                    break;
+9                }
+10            }
+11        }
+12        return count;
+13    }
+14}
