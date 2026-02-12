f03ef5ebdf2437ad20114e147367b76fcffee280
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-12 15:41:12 -0500

    File updated at 2/12/2026, 3:41:11 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,15 +1,24 @@
-# Last updated: 2/12/2026, 3:29:25 PM
+# Last updated: 2/12/2026, 3:41:11 PM
 1class Solution:
-2    def isPalindrome(self, x: int) -> bool:
-3        if x < 0:
-4            return False
-5        else:
-6            string = str(x)
-7            l = 0
-8            r = len(string)-1
-9            while(l < r):
-10                if (string[l] != string[r]):
-11                    return False
-12                l += 1
-13                r -= 1         
-14            return True
+2    def romanToInt(self, s: str) -> int:
+3        roman_dict = {
+4            'I': 1,
+5            'V': 5, 
+6            'X': 10,
+7            'L': 50, 
+8            'C': 100, 
+9            'D': 500, 
+10            'M': 1000
+11        }
+12        total = 0
+13        prev = ''
+14        for i in range(0, len(s)):
+15            total += roman_dict[s[i]]
+16            if ((s[i] == 'V' or s[i] == 'X') and prev == 'I'):
+17                total -= 2
+18            elif ((s[i] == 'L' or s[i] == 'C') and prev == 'X'):
+19                total -= 20
+20            elif ((s[i] == 'D' or s[i] == 'M') and prev == 'C'):
+21                total -= 200
+22            prev = s[i]
+23        return total
