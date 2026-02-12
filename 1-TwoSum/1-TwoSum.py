5fe6ee32c4f0735d1fd3908a0f812e230c19764f
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-12 15:29:26 -0500

    File updated at 2/12/2026, 3:29:25 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,8 +1,15 @@
-# Last updated: 2/12/2026, 3:15:31 PM
+# Last updated: 2/12/2026, 3:29:25 PM
 1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        for i in range(0, len(nums)):
-4            for j in range(i+1, len(nums)):
-5                if nums[i] + nums[j] == target:
-6                    return [i,j]
-7
+2    def isPalindrome(self, x: int) -> bool:
+3        if x < 0:
+4            return False
+5        else:
+6            string = str(x)
+7            l = 0
+8            r = len(string)-1
+9            while(l < r):
+10                if (string[l] != string[r]):
+11                    return False
+12                l += 1
+13                r -= 1         
+14            return True
