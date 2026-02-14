b8a01ddf85b78b09d97368a0186e41ff53120fc2
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-14 14:02:43 -0500

    File updated at 2/14/2026, 2:02:43 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,12 +1,24 @@
-# Last updated: 2/14/2026, 12:13:04 PM
+# Last updated: 2/14/2026, 2:02:43 PM
 1class Solution:
-2    def uniqueOccurrences(self, arr: List[int]) -> bool:
-3        nums = set()
-4        occurances = set()
-5        for num in arr:
-6            if num not in nums:
-7                occurances.add(arr.count(num))
-8                nums.add(num)
-9        if len(nums) == len(occurances):
-10            return True
-11        return False
+2    def canBeIncreasing(self, nums: List[int]) -> bool:
+3        if len(nums) == 2:
+4            return True
+5        for i in range(0, len(nums)):
+6            if i == 0:
+7                if self.isSorted(nums[1:len(nums)]):
+8                    return True
+9            elif i == len(nums):
+10                if self.isSorted(nums[0:len(nums)]):
+11                    return True
+12            else:
+13                if self.isSorted(nums[0:i] + nums[i+1:len(nums)]):
+14                    return True    
+15        return False
+16        
+17    def isSorted(self, nums: List[int]) -> bool:
+18        sorted_nums = sorted(nums)
+19        set_nums = set(nums)
+20        if nums == sorted_nums and (nums.count(nums[0]) != len(nums)) and len(set_nums) == len(nums):
+21            return True
+22        return False
+23
