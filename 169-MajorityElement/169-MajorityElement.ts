0b8e6e28693e408924f43e487904a0417c2a51f6
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-15 20:54:19 -0500

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.ts
@@ -0,0 +1,14 @@
+// Last updated: 2/15/2026, 8:54:17 PM
+function majorityElement(nums: number[]): number {
+    const m = new Map()
+
+    for (const n of nums){
+        if (m.has(n)) {
+            m.set(n, m.get(n) + 1)
+        } else {
+            m.set(n, 1)
+        }
+    }
+
+    return Array.from(m.entries()).sort((a, b) => b[1] - a[1])[0][0]
+};
