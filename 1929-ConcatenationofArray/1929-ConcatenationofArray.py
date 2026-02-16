ce5852d732593ad4f249e31e61a65e8c5c5996c7
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-16 14:38:51 -0500

    File updated at 2/16/2026, 2:38:50 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,14 +1,9 @@
-# Last updated: 2/16/2026, 2:32:56 PM
+# Last updated: 2/16/2026, 2:38:50 PM
 1class Solution:
-2    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-3        my_dict = {}
-4        sorted_nums = sorted(nums)
-5        count = 0
-6        for num in sorted_nums:
-7            if num not in my_dict:
-8                my_dict[num] = count
-9            count += 1
-10        res = []
-11        for num in nums:
-12            res.append(my_dict.get(num))
-13        return res
+2    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+3        num_set = set(nums)
+4        res = []
+5        for i in range(1, len(nums)+1):
+6            if i not in num_set:
+7                res.append(i)
+8        return res
