81bb2767c237b5d9ddcb029763d287b37e95c009
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-16 12:20:46 -0500

    File updated at 2/16/2026, 12:20:45 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,10 +1,12 @@
-# Last updated: 2/16/2026, 12:16:25 PM
+# Last updated: 2/16/2026, 12:20:45 PM
 1class Solution:
-2    def getConcatenation(self, nums: List[int]) -> List[int]:
-3        res = []
-4        for i in range(0, (len(nums))*2):
-5            if i > len(nums)-1:
-6                res.append(nums[i-(len(nums))])
-7            else:
-8                res.append(nums[i])
-9        return res
+2    def shuffle(self, nums: List[int], n: int) -> List[int]:
+3        pointer_one = 0
+4        pointer_two = n
+5        res = []
+6        while len(res) < len(nums):
+7            res.append(nums[pointer_one])
+8            res.append(nums[pointer_two])
+9            pointer_one += 1
+10            pointer_two += 1
+11        return res
