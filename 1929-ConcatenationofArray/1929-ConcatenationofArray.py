da89b35359ade6135741fbdfd303d4bf2e7296f1
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-16 14:32:56 -0500

    File updated at 2/16/2026, 2:32:56 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,14 +1,14 @@
-# Last updated: 2/16/2026, 12:35:38 PM
+# Last updated: 2/16/2026, 2:32:56 PM
 1class Solution:
-2    def findErrorNums(self, nums: List[int]) -> List[int]:
-3        dupl = 0
-4        miss = 0
-5        for i in range(1, len(nums)+1):
-6            if i not in nums:
-7                miss = i
-8            if nums.count(i) > 1:
-9                dupl = i
-10            if miss != 0 and dupl != 0:
-11                break
-12        return [dupl, miss]
-13
+2    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+3        my_dict = {}
+4        sorted_nums = sorted(nums)
+5        count = 0
+6        for num in sorted_nums:
+7            if num not in my_dict:
+8                my_dict[num] = count
+9            count += 1
+10        res = []
+11        for num in nums:
+12            res.append(my_dict.get(num))
+13        return res
