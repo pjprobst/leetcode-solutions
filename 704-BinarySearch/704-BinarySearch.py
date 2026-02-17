790fe8c72ff516d4ec1fdf864df60845d5d9674b
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-16 23:17:13 -0500

    File updated at 2/16/2026, 11:17:13 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,19 +1,15 @@
-# Last updated: 2/14/2026, 2:20:26 PM
+# Last updated: 2/16/2026, 11:17:13 PM
 1class Solution:
-2    def diagonalSum(self, mat: List[List[int]]) -> int:
-3        seen = []
-4        lsum = 0
-5        i = 0
-6        while i != len(mat):
-7            lsum += mat[i][i]
-8            seen.append([i,i])
-9            i += 1
-10
-11        rsum = 0
-12        j = 0
-13        while j != len(mat):
-14            if [j, len(mat[j]) - 1 - j] not in seen:
-15                rsum += mat[j][len(mat[j]) - 1 - j]
-16                seen.append([j, len(mat[j]) - 1 - j])
-17            j += 1
-18        return lsum + rsum
+2    def search(self, nums: List[int], target: int) -> int:
+3        low = 0
+4        high = len(nums)-1
+5        while low <= high:
+6            mid = (high + low)//2
+7            print(low, mid, high)
+8            if nums[mid] < target:
+9                low = mid + 1
+10            elif nums[mid] > target:
+11                high = mid -1
+12            else:
+13                return mid
+14        return -1
