7b5f9ff6a7b07132b96ba9fa88414e4b7c37835f
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-17 15:19:18 -0500

    File updated at 2/17/2026, 3:19:18 PM

--- 2541-MinimumOperationstoMakeArrayEqualII/2541-MinimumOperationstoMakeArrayEqualII.py
+++ 2541-MinimumOperationstoMakeArrayEqualII/2541-MinimumOperationstoMakeArrayEqualII.py
@@ -1,29 +1,25 @@
-# Last updated: 2/17/2026, 3:05:32 PM
+# Last updated: 2/17/2026, 3:19:18 PM
 1class Solution:
-2    def minOperations(self, nums1: List[int], nums2: List[int], k: int) -> int:
-3        total = 0
-4        addk = 0
-5        subk = 0
-6
-7        if k == 0:
-8            if nums1 != nums2:
-9                return -1
-10            else:
-11                return 0
-12
-13        for i in range(len(nums1)):
-14            num1 = nums1[i]
-15            num2 =nums2[i]
-16            if num2 - num1 < 0:
-17                subk += abs((num2 - num1)/k)
-18            elif num2 - num1 > 0:
-19                addk += (num2 - num1)/k
-20            total += (num2 - num1)/k
-21            if subk % 1 != 0 or addk % 1 != 0:
-22                return -1
-23        if total != 0 or subk != addk or subk % 1 != 0:
-24            return -1
-25        else:
-26            return int(subk)
-27        
-28
+2    def generate(self, numRows: int) -> List[List[int]]:
+3        '''
+4                1
+5               1 1
+6              1 2 1
+7             1 3 3 1
+8            1 4 6 4 1
+9          1 5 10 10 5 1
+10         1 6 15 20 15 6 1
+11
+12        '''
+13        res = []
+14        for i in range(numRows):
+15            row = []
+16            for j in range(i+1):
+17                if i == 0:
+18                    row.append(1)
+19                elif j-1 in range(len(res[i-1])) and j in range(len(res[i-1])):
+20                    row.append(res[i-1][j-1] + res[i-1][j])
+21                else:
+22                    row.append(1)
+23            res.append(row)
+24        return res
