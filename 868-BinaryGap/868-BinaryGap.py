12d0851672dd0262e48f5fd3774c64c7e5c1603a
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-22 00:50:23 -0500

    File updated at 2/22/2026, 12:50:22 AM

--- 868-BinaryGap/868-BinaryGap.py
+++ 868-BinaryGap/868-BinaryGap.py
@@ -1,15 +1,14 @@
-# Last updated: 2/22/2026, 12:50:17 AM
+# Last updated: 2/22/2026, 12:50:22 AM
 1class Solution:
 2    def binaryGap(self, n: int) -> int:
 3        max_distance = 0
 4        count = 0
-5        print(bin(n)[2:])
-6        for num in bin(n)[2:]:
-7            if count != 0 and num == '1':
-8                max_distance = max(max_distance, count)
-9                count = 0
-10            if count == 0 and num == '1':
-11                count += 1
-12            elif count != 0 and num == '0':
-13                count += 1
-14        return max_distance
+5        for num in bin(n)[2:]:
+6            if count != 0 and num == '1':
+7                max_distance = max(max_distance, count)
+8                count = 0
+9            if count == 0 and num == '1':
+10                count += 1
+11            elif count != 0 and num == '0':
+12                count += 1
+13        return max_distance
