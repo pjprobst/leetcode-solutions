c6777818b86cf4756b1dcc18df64d7b4251ed35a
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-24 22:45:17 -0500

    File updated at 2/24/2026, 10:45:17 PM

--- 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.py
+++ 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.py
@@ -1,14 +1,14 @@
-# Last updated: 2/24/2026, 10:41:44 PM
+# Last updated: 2/24/2026, 10:45:17 PM
 1class Solution:
 2    def sortByBits(self, arr: List[int]) -> List[int]:
 3        arr = sorted(arr)
-4        binarr = []
+4        binarr = [[0,0]] * len(arr)
 5        for i, num in enumerate(arr):
-6            binarr.append([i, bin(num).count("1")])
+6            binarr[i] = [i, bin(num).count("1")]
 7        binarr = sorted(binarr, key= lambda x: x[1])
-8        res = []
-9        for j in binarr:
-10            res.append(arr[j[0]])
+8        res = [0] * len(arr)
+9        for k, j in enumerate(binarr):
+10            res[k] = arr[j[0]]
 11        return res
 12
 13
