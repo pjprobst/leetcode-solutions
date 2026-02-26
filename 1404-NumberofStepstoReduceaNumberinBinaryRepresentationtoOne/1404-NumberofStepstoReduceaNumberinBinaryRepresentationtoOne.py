464473a9ac91418c2c9e4190fa69e11b5ad829a7
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-25 19:14:12 -0500

    File updated at 2/25/2026, 7:14:11 PM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
@@ -1,21 +1,20 @@
-# Last updated: 2/25/2026, 7:14:01 PM
+# Last updated: 2/25/2026, 7:14:11 PM
 1class Solution:
 2    def numSteps(self, s: str) -> int:
 3        count = 0
 4        num = self.binaryToInt(s)
-5        print(num)
-6        while num != 1:
-7            if num % 2 == 0:
-8                num = num // 2
-9            else:
-10                num += 1
-11            count += 1
-12        return count
-13    def binaryToInt(self, s: str) -> int:
-14        total = 0
-15        add = int(math.pow(2, len(s)-1))
-16        for l in s:
-17            if l == "1":
-18                total += int(add)
-19            add = int(add // 2)
-20        return total
+5        while num != 1:
+6            if num % 2 == 0:
+7                num = num // 2
+8            else:
+9                num += 1
+10            count += 1
+11        return count
+12    def binaryToInt(self, s: str) -> int:
+13        total = 0
+14        add = int(math.pow(2, len(s)-1))
+15        for l in s:
+16            if l == "1":
+17                total += int(add)
+18            add = int(add // 2)
+19        return total
