a8bf9becd9592974b3271c7601501f119480608c
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-02-25 20:11:19 -0500

    File updated at 2/25/2026, 8:11:19 PM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
@@ -1,20 +1,14 @@
-# Last updated: 2/25/2026, 7:14:34 PM
+# Last updated: 2/25/2026, 8:11:19 PM
 1class Solution:
-2    def numSteps(self, s: str) -> int:
-3        count = 0
-4        num = self.binaryToInt(s)
-5        while num != 1:
-6            if num % 2 == 0:
-7                num = num // 2
-8            else:
-9                num += 1
-10            count += 1
-11        return count
-12    def binaryToInt(self, s: str) -> int:
-13        total = 0
-14        add = int(math.pow(2, len(s)-1))
-15        for l in s:
-16            if l == "1":
-17                total += add
-18            add = add // 2
-19        return total
+2    def kLengthApart(self, nums: List[int], k: int) -> bool:
+3        count = None
+4        for i in range(len(nums)):
+5            if count is None and nums[i] is 1:
+6                count = 0
+7            elif nums[i] is 1 and count is not None:
+8                if count < k:
+9                    return False
+10                count = 0
+11            elif nums[i] is 0 and count is not None:
+12                count += 1
+13        return True
