1fe0b776b2c5a36ff5ccb2107298b6a65b19b08f
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-03-05 12:12:06 -0500

    File updated at 3/5/2026, 12:12:06 PM

--- 3158-FindtheXORofNumbersWhichAppearTwice/3158-FindtheXORofNumbersWhichAppearTwice.py
+++ 3158-FindtheXORofNumbersWhichAppearTwice/3158-FindtheXORofNumbersWhichAppearTwice.py
@@ -1,15 +1,14 @@
-# Last updated: 3/5/2026, 12:11:57 PM
+# Last updated: 3/5/2026, 12:12:06 PM
 1class Solution:
 2    def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
 3        occ = []
 4        for i in range(len(nums)):
 5            if nums[i] == x:
 6                occ.append(i)
-7        print(occ)
-8        ans = []
-9        for query in queries:
-10            if query not in range(len(occ)+1):
-11                ans.append(-1)
-12            else:
-13                ans.append(occ[query-1])
-14        return ans
+7        ans = []
+8        for query in queries:
+9            if query not in range(len(occ)+1):
+10                ans.append(-1)
+11            else:
+12                ans.append(occ[query-1])
+13        return ans
