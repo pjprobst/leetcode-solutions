0bf656323acaef96540c82e30c2a0ae4c35c884f
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-03-04 20:15:49 -0500

    File updated at 3/4/2026, 8:15:49 PM

--- 1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.py
+++ 1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.py
@@ -1,14 +1,11 @@
-# Last updated: 3/4/2026, 8:15:21 PM
+# Last updated: 3/4/2026, 8:15:49 PM
 1class Solution:
 2    def minOperations(self, s: str) -> int:
 3        if len(s) % 2 == 0:
-4            poss1 = "01" * (len(s)//2)
-5            poss2 = "10" * (len(s)//2)
-6        else:
-7            poss1 = ("01" * (len(s)//2)) + "0"
-8            poss2 = ("10" * (len(s)//2)) + "1"
+4            poss = "01" * (len(s)//2)
+5        else:
+6            poss = ("01" * (len(s)//2)) + "0"
+7
+8        count = sum(1 for a, b in zip(s, poss) if a != b)
 9
-10        count1 = sum(1 for a, b in zip(s, poss1) if a != b)
-11        count2 = sum(1 for a, b in zip(s, poss2) if a != b)
-12
-13        return min(count1, len(s) - count1)
+10        return min(count, len(s) - count)
