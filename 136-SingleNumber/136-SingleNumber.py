da0d8d49406b5c2948235498e58ca93238e4153b
pjprobst <111828284+pjprobst@users.noreply.github.com>
2026-03-10 17:52:09 -0400

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.py
@@ -0,0 +1,10 @@
+# Last updated: 3/10/2026, 5:52:10 PM
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return seen.pop()
