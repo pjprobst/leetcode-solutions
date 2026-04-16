affde6a68d3a5a10384e072dca8e60790fd4cc97
Preston Probst <111828284+pjprobst@users.noreply.github.com>
2026-04-16 15:38:14 -0400

    File updated at 4/16/2026, 3:38:13 PM

--- 3488-ClosestEqualElementQueries/3488-ClosestEqualElementQueries.py
+++ 3488-ClosestEqualElementQueries/3488-ClosestEqualElementQueries.py
@@ -1,30 +1,33 @@
-# Last updated: 4/16/2026, 3:27:48 PM
+# Last updated: 4/16/2026, 3:38:13 PM
 1class Solution:
 2    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
 3        res = []
-4        mapd = dict()
-5        for i in range(len(nums)):
-6            if nums[i] not in mapd:
-7                mapd[nums[i]] = [i]
-8            else:
-9                mapd[nums[i]].append(i)
-10
-11        for q in queries:
-12            mini = -1
-13            if nums[q] in mapd and len(mapd[nums[q]]) > 1:
-14                pos = self.bisect_left(mapd[nums[q]], q)
-15                l = mapd[nums[q]][(pos - 1) % len(mapd[nums[q]])]
-16                r = mapd[nums[q]][(pos + 1) % len(mapd[nums[q]])]
-17                mini = min(abs(q-l), abs(len(nums)-q + l), abs(len(nums)-l + q), abs(q-r), abs(len(nums)-q + r), abs(len(nums)-r + q))
-18            res.append(mini)         
-19        return res
-20    
-21    def bisect_left(self, arr, target) -> int:
-22        lo, hi = 0, len(arr)
-23        while lo < hi:
-24            mid = (lo + hi) // 2
-25            if arr[mid] < target:
-26                lo = mid + 1
-27            else:
-28                hi = mid
-29        return lo
+4        mapd = {}
+5        loc = {} 
+6
+7        qset = set(queries)
+8        for i in range(len(nums)):
+9            if nums[i] not in mapd:
+10                mapd[nums[i]] = [i]
+11                if i in qset:
+12                    loc[i] = 0
+13            else:
+14                if i in qset:
+15                    loc[i] = len(mapd[nums[i]]) 
+16                mapd[nums[i]].append(i)
+17
+18        n = len(nums)
+19        for q in queries:
+20            mini = -1
+21            positions = mapd[nums[q]]
+22            if len(positions) > 1:
+23                pos = loc[q]
+24                l = positions[(pos - 1) % len(positions)]
+25                r = positions[(pos + 1) % len(positions)]
+26                mini = min(
+27                    abs(q-l), abs(n-q+l), abs(n-l+q),
+28                    abs(q-r), abs(n-q+r), abs(n-r+q)
+29                )
+30            res.append(mini)
+31
+32        return res
